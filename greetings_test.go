50a4cb6478c6381a1670da9b1b1a79e8493c10a7
krava <krava@root.cpi>
2022-04-03 16:56:29 +0300

    The homework is done for GoHello

---
+++ greetings_test.go
@@ -0,0 +1,23 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Erast"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Erast")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Erast") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg!= "" || err ==nil {
+		t.Fatalf(`Hello("") = %q, %v, want "",error`,msg,err)
+	}
+}
